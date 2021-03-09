e1aff22bffbd636bb04be911c05978d2d4e6964e
Volodymyr Reient <4reient4@gmail.com>
2021-03-09 13:01:59 +0200

    added link to repo

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,14 +1,18 @@
 function Footer() {
     return (
         <footer className="page-footer blue lighten-2">
-        
-        <div className="footer-copyright">
-          <div className="container">
-          © {new Date().getFullYear()} Copyright Text
-          <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
-          </div>
-        </div>
-      </footer>
-    )
+            <div className="footer-copyright">
+                <div className="container">
+                    © {new Date().getFullYear()} Copyright Text
+                    <a
+                        className="grey-text text-lighten-4 right"
+                        href="https://github.com/VolodymyrReient/films-app"
+                    >
+                        Repo
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
 }
 export default Footer;
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,20 @@
 function Header() {
-     return(
+    return (
         <nav className="blue darken-4">
-        <div className="nav-wrapper">
-          <a href="!#" className="brand-logo">React Movies</a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="!#">Repo</a></li>
-          </ul>
-        </div>
-      </nav>
-     )
+            <div className="nav-wrapper">
+                <a href="!#" className="brand-logo">
+                    React Movies
+                </a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li>
+                        <a href="https://github.com/VolodymyrReient/films-app">
+                            Repo
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export default Header;
